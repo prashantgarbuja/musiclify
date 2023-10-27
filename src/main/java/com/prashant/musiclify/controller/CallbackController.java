4f8d0ed1ceb0fa70aaa9a1677a42b9323f99e461
Prashant Garbuja <prashantmaha@yahoo.com>
2023-10-28 04:41:37 +1100

    Added sample video

--- src/main/java/com/prashant/musiclify/controller/CallbackController.java
+++ src/main/java/com/prashant/musiclify/controller/CallbackController.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @AllArgsConstructor
