0beef1ee33bab4351819cac970c7f1d5f626a4c8
Prashant Garbuja <prashantmaha@yahoo.com>
2023-10-29 19:10:31 +1100

    add a check for token expiration time

--- src/main/java/com/prashant/musiclify/controller/CallbackController.java
+++ src/main/java/com/prashant/musiclify/controller/CallbackController.java
@@ -17,27 +17,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 @AllArgsConstructor
 public class CallbackController {
 
-	private final AccessTokenService accessToken;
-	private final SpotifyUrlService spotifyUrlService;
+    private final AccessTokenService accessToken;
+    private final SpotifyUrlService spotifyUrlService;
 
-	@GetMapping(value = ApiPath.CALLBACK, produces = MediaType.TEXT_HTML_VALUE)
-	public String handleCallback(@RequestParam(value = "code", required = false) final String code,
-									   @RequestParam(value = "error", required = false) final String error, final Model model,
-									   HttpServletRequest request) {
+    @GetMapping(value = ApiPath.CALLBACK, produces = MediaType.TEXT_HTML_VALUE)
+    public String handleCallback(@RequestParam(value = "code", required = false) final String code,
+                                 @RequestParam(value = "error", required = false) final String error, final Model model,
+                                 HttpServletRequest request) {
 
-		HttpSession session = request.getSession(); // Get HttpSession from the HttpServletRequest
-		//System.out.println("Code is "+code+" Error is "+error+" Model is "+model+" Session is "+session);
+        HttpSession session = request.getSession(); // Get HttpSession from the HttpServletRequest
+        //System.out.println("Code is "+code+" Error is "+error+" Model is "+model+" Session is "+session);
 
-		if (error != null){
-			model.addAttribute("url", spotifyUrlService.getAuthorizationURL());
-			return Template.CALLBACK_FAILURE;
-		}
+        if (error != null) {
+            model.addAttribute("url", spotifyUrlService.getAuthorizationURL());
+            return Template.CALLBACK_FAILURE;
+        }
 
 //		session.setAttribute("code", code);
-		String token = accessToken.getToken(code);
+        String token = accessToken.getToken(code);
+        String expires_in = accessToken.getExpiresIn();
+        Long expiration_time = System.currentTimeMillis() + Integer.parseInt(expires_in) * 1000L;
+        session.setAttribute("accessToken", token);
+        session.setAttribute("expiration_time", expiration_time);
 
-		session.setAttribute("accessToken", token);
-
-		return "redirect:"+ApiPath.REDIRECT;
-	}
+        return "redirect:" + ApiPath.REDIRECT;
+    }
 }
